cc53ce2cd5511af1385715890e7a7f30a78e97ac
Apirak Fakin <136418361+Apirak19@users.noreply.github.com>
2023-06-16 10:03:03 +0700

    SortingBanknoteTHB.py

--- SortingBanknoteTHB.py
+++ SortingBanknoteTHB.py
@@ -1,3 +1,6 @@
+import math
+
+print('-'*70)
 print("This is a program for identifying the number of Thai banknote used for the amount of money you provide")
 
 money = int(input("Enter the amount of money:"))
@@ -35,3 +38,24 @@ print("1coin: ", coin1, "= ", coin1*1)
 print("TotalNote: ",(note1k+note500+note100+note50+note20))
 print("TotalCoin: ", (coin10+coin5+coin1))
 print("TotalAmountofMoney: ", money)
+
+print('-'*70)
+
+print("The following is the number of each type of note and coin you need for the amount of money provided")
+aNote1k = math.ceil(money/ 1000)
+aNote500 = math.ceil(money/ 500)
+aNote100 = math.ceil(money/ 100)
+aNote50 = math.ceil(money/ 50)
+aNote20 = math.ceil(money/ 20)
+aCoin10 = math.ceil(money/ 10)
+aCoin5 = math.ceil(money/ 5)
+aCoin1 = math.ceil(money)
+
+print("You need to use ", aNote1k, "1kNote, the change you will get: ", abs(1000-(money%1000)))
+print("You need to use ", aNote500, "500Note, the change you will get: ", abs(500-(money%500)))
+print("You need to use ", aNote100, "100Note, the change you will get: ", abs(100-(money%100)))
+print("You need to use ", aNote50, "50Note, the change you will get: ", abs(50-(money%50)))
+print("You need to use ", aNote20, "20Note, the change you will get: ", abs(20-(money%20)))
+print("You need to use ", aCoin10, "10Coin, the change you will get: ", abs(10-(money%10)))
+print("You need to use ", aCoin5, "5Coin, the change you will get: ", abs(5-(money%5)))
+print("You need to use ", aCoin1, "1Coin, the change you will get: 0")
